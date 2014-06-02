bf07778a8d59ab6655c921c187028ffb01d26ef2
Yassine Guedidi <yassine@guedidi.com>
2014-06-02 20:36:09 +0200

    Add redirect to post if only one post in the query

--- functions.php
+++ functions.php
@@ -63,3 +63,13 @@ if ( class_exists( 'TVJussieu' ) ) {
 
 	$tvj = new TVJussieu();
 }
+
+function redirect_to_post(){
+    global $wp_query;
+    if( is_archive() && $wp_query->post_count == 1 ){
+        the_post();
+        $post_url = get_permalink();
+        wp_redirect( $post_url );
+    }
+}
+add_action('template_redirect', 'redirect_to_post');
