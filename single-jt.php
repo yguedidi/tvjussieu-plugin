2a47cae7788ccb19352578a7360e80225020147a
Yassine Guedidi <yassine@guedidi.com>
2014-06-03 11:17:38 +0200

    JT navigation

--- single-jt.php
+++ single-jt.php
@@ -23,7 +23,29 @@ get_header();
 			get_template_part( 'partials/content', 'jt' );
 
 			// Previous/next post navigation.
-			twentyfourteen_post_nav();
+			// Don't print empty markup if there's nowhere to navigate.
+			$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+			$next     = get_adjacent_post( false, '', false );
+
+			if ( ! $next && ! $previous ) {
+				return;
+			}
+
+			?>
+			<nav class="navigation post-navigation" role="navigation">
+				<h1 class="screen-reader-text"><?php _e( 'JT navigation', 'tvjussieu' ); ?></h1>
+				<div class="nav-links">
+					<?php
+					if ( is_attachment() ) :
+						previous_post_link( '%link', __( '<span class="meta-nav">Published In</span>%title', 'tvjussieu' ) );
+					else :
+						previous_post_link( '%link', __( '<span class="meta-nav">JT précédent</span>%title', 'tvjussieu' ) );
+						next_post_link( '%link', __( '<span class="meta-nav">JT suivant</span>%title', 'tvjussieu' ) );
+					endif;
+					?>
+				</div><!-- .nav-links -->
+			</nav><!-- .navigation -->
+			<?php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
