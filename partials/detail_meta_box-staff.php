07dbfc7c0355b9582179bed52edda9db7cf1445a
Yassine Guedidi <yassine@guedidi.com>
2014-06-13 16:48:20 +0200

    Make staff firstname and lastname required in HTML5

--- partials/detail_meta_box-staff.php
+++ partials/detail_meta_box-staff.php
@@ -1,7 +1,7 @@
-<p><strong><?php echo __( 'Prénom', 'tvjussieu' ); ?></strong></p>
-<p><input type="text" name="staff_firstname" value="<?php echo esc_attr( $firstname ); ?>" /></p>
-<p><strong><?php echo __( 'Nom', 'tvjussieu' ); ?></strong></p>
-<p><input type="text" name="staff_lastname" value="<?php echo esc_attr( $lastname ); ?>" /></p>
+<p><strong><?php echo __( 'Prénom', 'tvjussieu' ); ?>*</strong></p>
+<p><input type="text" name="staff_firstname" value="<?php echo esc_attr( $firstname ); ?>" required /></p>
+<p><strong><?php echo __( 'Nom', 'tvjussieu' ); ?>*</strong></p>
+<p><input type="text" name="staff_lastname" value="<?php echo esc_attr( $lastname ); ?>" required /></p>
 <p><strong><?php echo __( 'Surnom', 'tvjussieu' ); ?></strong></p>
 <p><input type="text" name="staff_nickname" value="<?php echo esc_attr( $nickname ); ?>" /></p>
 <p><strong><?php echo __( 'Promo', 'tvjussieu' ); ?></strong></p>
